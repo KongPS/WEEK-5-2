0b2a348a5981ea80b5fff2ab6662761980f67b76
Xcaliburth <88648253+Xcaliburth@users.noreply.github.com>
2021-09-05 21:50:33 +0700

    Update Source.cpp

--- WEEK 5-2/Source.cpp
+++ WEEK 5-2/Source.cpp
@@ -6,5 +6,6 @@ int main()
 	scanf_s("%d%d%d%d%d%d%d%d%d%d", &a,&b,&c,&d,&e,&f,&g,&h,&i,&j);
 	printf("%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n", a, b, c, d, e, f, g, h, i, j);
 	sum = (a + b + c + d + e + f + g + h + i + j) / 10;
-	printf("%d", sum);
+	printf("Average Hight is : %d", sum);
+
 }
